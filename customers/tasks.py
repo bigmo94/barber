5054822ee5b57d75136464af447e16a18e61e9d7
bigmo94 <m.rezaie94@yahoo.com>
2022-01-09 10:43:41 +0330

    Create tasks to send message

--- customers/tasks.py
+++ customers/tasks.py
@@ -6,3 +6,8 @@ from customers.utils import send_message
 @shared_task
 def send_verification_code_task(user_phone, verification_code):
     send_message({'receptor': user_phone, 'message': verification_code})
+
+
+@shared_task
+def send_forget_pass_code_task(user_phone, pass_code):
+    send_message({'receptor': user_phone, 'message': pass_code})
